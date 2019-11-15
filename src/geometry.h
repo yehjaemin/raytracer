b9ccf6b8a49b60b02cf4f90b30bebec5019ec545
yehjaemin <yeh.jaemin@gmail.com>
2019-11-14 18:35:16 -0800

    Add 3d vector

--- src/geometry.h
+++ src/geometry.h
@@ -49,9 +49,8 @@ public:
 
     T operator[](int i) {
         assert 0 <= i && i <= 1;
-        if (i == 0) {
+        if (i == 0)
             return x;
-        }
         return y;
     }
 
@@ -71,7 +70,70 @@ public:
     Vector3() : x(0), y(0), z(0);
     Vector3(T i, T j, T k) : x(i), y(j), z(k);
 
-    
+    Vector3<T> operator+(const Vector3<T> &v) const {
+        return Vector3(x + v.x, y + v.y, z + v.z);
+    }
+
+    Vector3<T> &operator+=(const Vector3<T> &v) const {
+        x += v.x;
+        y += v.y;
+        z += v.z;
+        return *this;
+    }
+
+    Vector3<T> operator-(const Vector3<T> &v) const {
+        return Vector3(x - v.x, y - v.y, z - v.z);
+    }
+
+    Vector3<T> operator-=(const Vector3<T> &v) const {
+        v -= v.x;
+        y -= v.y;
+        z -= v.z;
+        return *this;
+    }
+
+    template <typename U> operator*(U u) const {
+        return Vector3(u * x, u * y, u * z);
+    }
+
+    template <typename U> operator*=(U u) const {
+        x *= u;
+        y *= u;
+        z *= u;
+        return *this;
+    }
+
+    template <typename U> operator/(U u) const {
+        return Vector3(x / u, y / u, z / u);
+    }
+
+    template <typename U> operator/=(U u) const {
+        x /= u;
+        y /= u;
+        z /= u;
+        return *this;
+    }
+
+    Vector3<T> operator-() const {
+        return Vector3(-x, -y, -z);
+    }
+
+    T operator[](int i) {
+        assert(0 <= i && i <= 2);
+        if (i == 0)
+            return x;
+        if (i == 1)
+            return y;
+        return z;
+    }
+
+    bool operator==(const Vector3<T> &v) const {
+        return x == v.x && y == v.y && z == v.z;
+    }
+
+    bool operator!=(const Vector3<T> &v) const {
+        return x != v.x && y != v.y && z != v.z;
+    }
 
     T x, y, z;
 };
