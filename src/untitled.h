eb5daecced7c60d600c1b042c15b52a10408cc12
yehjaemin <frank@franks-macbook-pro.local>
2019-12-20 13:41:36 -0800

    Add writing to disk

--- src/untitled.h
+++ src/untitled.h
@@ -5,3 +5,17 @@ inline float lerp(float t, float a, float b) {
     return (1 - t) * a + t * b;
 }
 } // namespace untitled
+
+// --------------------
+// tiny ray tracer ver.
+// --------------------
+
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include "geometry.h"
+
+void render();
+int main();
