4dc15e8f89ad28299ae4cea1dd62c6e3729d5585
yehjaemin <41862298+yehjaemin@users.noreply.github.com>
2020-01-15 21:29:35 -0800

    Add initRay() and remove castRay()

--- src/cameras/orthographic.cc
+++ src/cameras/orthographic.cc
@@ -2,4 +2,10 @@
 
 OrthographicCamera::OrthographicCamera(const Transform &cameraToWorld, const Bounds2f &screenWindow) : ProjectiveCamera(cameraToWorld, Transform(), screenWindow) {} // default transform is identity which is a simple orthographic transform
 
-void OrthographicCamera::castRay(const Point2f &fp, Ray3f *ray) const {}
+void OrthographicCamera::initRay(const Point2f &sample, Ray3f *ray) const {
+    Point3f pRaster = Point3f(pixel.x, pixel.y, 0);
+    Point3f pCamera = rasterToCamera(pRaster);
+
+    *ray = Ray3f(pCamera, Vector3f(0, 0, 1); // orthographic
+    *ray = cameraToWorld(*ray);
+}
